df2cb55d9f30d0f82baf1d1df3b19c99d7b4750b
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2024-10-09 11:55:45 -0700

    Mod 1 Critical Thinking package completed

--- ITS-320/Module_1/ITS320_CTA1_Option2.py
+++ ITS-320/Module_1/ITS320_CTA1_Option2.py
@@ -1,23 +1,40 @@
-#Bryan Coss Y Leon Ortega
-#Module 1 Critical thinking 
-#ITS 320
+                                                #Create a Python Application
 
 
-#Read two integers and print three lines. The first line should contain integer division, //, 
-#the second line should contain float division, /, and the third line should contain modulo division, %.
+                                                # Bryan Coss Y Leon Ortega
+                                                # Colorado State University, Global
+                                                # ITS-320-1
+                                                # Ray Fernandez
+                                                # October 13, 2024
+
+
+
+# This program will Read two integers and print three lines. 
+# The first line should contain integer division, //,
+# the second line should contain float division, /,
+# and the third line should contain modulo division, %.
 #You do not need to perform any rounding or formatting operations.0
 #Input Format The first line contains the first integer. The second line contains the second integer.
 #Output Format Print the three lines as described above.
-#Sample Input43
-#Sample Output11.33333333333333331
+#Sample Input
+# 4
+# 3
+#Sample Output
+# 1
+# 1.3333333333333333
+# 1
 
-int1 = int(input("Enter the first integer "))
+# Psuedocode
+# int1 = User_input
+# int2 = User_input
+# print int1 // int2 (integer division)
+# print int1 / int2  (Float division)
+# print int1 % int2  (Modular division)
 
-int2 = int(int1 / 10)  #this will grab the 10s
-int3 = int1 % 10 #this will grab the 1s
 
-div = int(int2 / int3)
-flo = float (int2 / int3)
-mod = int2 % int3
+int1 = int(input())
+int2 = int(input())
 
-print(f"{div}{flo}{mod}")
+print(int1 // int2 )
+print(int1 / int2 )
+print(int1 % int2 )
