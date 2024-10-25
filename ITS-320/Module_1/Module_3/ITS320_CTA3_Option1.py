78defff23dd8633110fa577dd751ceedd7dfeafd
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2024-10-25 16:07:36 -0700

    Mod3 done

---
+++ ITS-320/Module_1/Module_3/ITS320_CTA3_Option1.py
@@ -0,0 +1,45 @@
+#-------------------------------------------
+# Program Name: Creating a Program to Calculate the Value of a Ferrari
+# Author: Bryan Coss Y Leon Ortega 
+# Date: 10/25
+#-------------------------------------------
+# Pseudocode: 
+# Prompt for user to input a year, the input has to be an int
+# from a table, The table has values of years from 1962 - 2014
+# We will look into the table and return the dollar value 
+# 
+#-------------------------------------------
+# Year      Value
+# 1962-1964	$18,500
+# 1965-1968	$6,000
+# 1969-1971	$12,000
+# 1972-1975	$48,000
+# 1976-1980	$200,000
+# 1981-1985	$650,000
+# 1986-2012	$35,000,000
+# 2013-2014	$52,000,000
+#
+# Program Inputs: 1986
+# Program Outputs: 35,000,000
+#-------------------------------------------
+
+year =  int(input('Enter year:\n'))
+
+if 1962 <= year <=1964 :
+    print('$', 18500)
+elif 1965 <= year <= 1968:
+    print('$', 6000)
+elif 1969 <= year <= 1971:
+    print('$',12000)
+elif 1972 <= year <= 1975:
+    print('$', 48000)
+elif 1976 <= year <= 1980:
+    print('$', 200000)
+elif 1981 <= year <= 1985:
+    print('$', 650000)
+elif 1986 <= year <= 2012:
+    print('$', 35000000)
+elif 2013 <= year <= 2014:
+    print('$',52000000)
+else:
+    print('Not a valid year')
