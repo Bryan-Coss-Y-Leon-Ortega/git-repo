8a969552a7e59787ef61e3ea5fc5487559497f48
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2024-10-25 07:12:39 -0700

    Mod 3 Critical Thinking complete

---
+++ ITS-320/Module_1/Module_2/mod_discussion3.py
@@ -0,0 +1,7 @@
+score = 49
+if (score > 70):
+ print(f"{score} is passing")
+elif(score <= 70 and score >50):
+ print(f"{score} is passing, but not by much")
+else:
+ print(f"{score} is a not passing score for this class")
