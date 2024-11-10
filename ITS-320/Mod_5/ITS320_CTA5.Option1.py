c03c19386af10c3481479fae750601fe6bacb772
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2024-11-10 14:31:09 -0700

    Week 5, almost done, needs updates.

---
+++ ITS-320/Mod_5/ITS320_CTA5.Option1.py
@@ -0,0 +1,12 @@
+
+def string_Reverse(str1, str2, str3):
+
+    return str3 + str2 + str1
+
+String1 = input("Please input a string: ")
+String2 = input("Please input a string: ")
+String3 = input("Please input a string: ")
+
+result = string_Reverse(String1, String2, String3)
+
+print(result)
