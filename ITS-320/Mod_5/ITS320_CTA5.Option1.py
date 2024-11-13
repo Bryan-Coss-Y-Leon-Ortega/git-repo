67f1def7b6beb98009d49417bc9ac3ed7201f5e3
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2024-11-12 19:54:59 -0700

    Week 5, python program needs comments

--- ITS-320/Mod_5/ITS320_CTA5.Option1.py
+++ ITS-320/Mod_5/ITS320_CTA5.Option1.py
@@ -1,8 +1,22 @@
+#-------------------------------------------
+# Program Name: Option #1: String Values in Reverse Order
+# Author: Bryan Coss Y Leon 
+# Date:
+#-------------------------------------------
+# Pseudocode: [See suggestions below]
+#-------------------------------------------
+# Program Inputs: [See suggestions below]
+# Program Outputs: [See suggestions below]
+#-------------------------------------------
+
 
 def string_Reverse(str1, str2, str3):
 
-    return str3 + str2 + str1
+    
 
+    return str3[::-1]+ " " + str2[::-1]+ " " + str1[::-1]
+
+print("This program will intake 3 strings and return it in a concatenated reversed order. \n")
 String1 = input("Please input a string: ")
 String2 = input("Please input a string: ")
 String3 = input("Please input a string: ")
