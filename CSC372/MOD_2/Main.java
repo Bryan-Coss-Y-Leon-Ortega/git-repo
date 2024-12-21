c897a083ceaaf6693041ec2339861668a1a778c7
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2024-12-20 18:41:55 -0700

    Mod_2 create the BankBalance class

---
+++ CSC372/MOD_2/Main.java
@@ -0,0 +1,20 @@
+package CSC372.MOD_2;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+    
+    JFrame newFrame = new JFrame();
+
+    newFrame.setSize(450, 450);
+    newFrame.setTitle("Bank of Bryan");
+
+    newFrame.setVisible(true);
+    newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    
+    
+
+}
