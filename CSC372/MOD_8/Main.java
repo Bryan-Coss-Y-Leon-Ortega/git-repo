80123a0d11d9bc960a04386107c3b011f9b1fbac
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2025-01-30 20:29:50 -0700

    Mod_8 started

--- CSC372/MOD_8/Main.java
+++ CSC372/MOD_8/Main.java
@@ -1,9 +1,12 @@
 package CSC372.MOD_8;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main extends Algorithm{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         LinkedList<Car> garage = new LinkedList<Car>();
 
         Car chevy = new Car("Chevy", "Cobalt", 2007);
@@ -16,6 +19,36 @@ public class Main extends Algorithm{
 
         numSort(garage);
         System.out.println(garage.toString());
+
+        FileInputStream fileInput = null;
+        Scanner scan = null;
+        String make;
+        String model;
+        double mpg;
+
+        //Attempt at opening file
+        System.out.println("Opening CarList.txt");
+        fileInput = new FileInputStream("C:\\Users\\bryan\\git-repo\\CSC372\\MOD_8\\CarList.txt");
+        scan = new Scanner(fileInput);
+
+        make = scan.next();
+        model = scan.next();
+        mpg = scan.nextDouble();
+
+        System.out.println(make);
+        System.out.println(model);
+        System.out.println(mpg);
+
+        Car newCar = new Car(make, model, mpg);
+        garage.add(newCar);
+        System.out.println(garage.toString());
+
+
+        fileInput.close();
+        scan.close();
+
+
+
 
     }
 }
