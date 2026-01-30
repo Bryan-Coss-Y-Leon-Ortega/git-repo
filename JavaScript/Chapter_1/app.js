4ee562186aca15c2b564edb996544859f99a89a4
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2026-01-30 13:47:29 -0700

    Chapter 1 completed!

---
+++ JavaScript/Chapter_1/app.js
@@ -0,0 +1,23 @@
+/*
+Author: Bryan Coss Y Leon
+Date: 1/30/2026
+Purpose: 
+    This will coincide with a text book for practice.
+    This will be the first prohject in many project in learning javascript.
+    This project is teaches me how to create a html page and link JS pages to it such as console logs and alerts
+    I will also be using variables      
+
+*/
+
+console.log("Hello from the app.js file");
+let a = 10; //variable a is 10
+console.log(a);
+
+// this is a comment
+
+/*
+Commenting in JS is the same as commenting in Java. 
+
+I believe this only applies to the JS and not to HTML
+
+*/
---
+++ JavaScript/Chapter_1/prompt.js
@@ -0,0 +1,9 @@
+/*
+Purpose:
+    Here we are learning how to prompt to a user
+    This will be linked to page3.html
+
+*/
+
+prompt("Hi, How are you?");
+console.log(Math.random());
