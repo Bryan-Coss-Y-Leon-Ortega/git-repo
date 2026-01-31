6a5dcaf61ca6688ff7d5c1c7c0a0f153de67b3ef
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2026-01-30 17:50:38 -0700

    Chapter 3 in progress

---
+++ JavaScript/Chapter_3/practice.js
@@ -0,0 +1,19 @@
+let arr1 = ["Black", "Blue", "White"];
+console.log(arr1);
+
+cars = ["Toyota", "Volkswagon","Chevy"];
+console.log(cars[0]);
+
+colors = ["black", "orange", "pink"]
+booleans = [true, false, false, true];
+emptyArray = [];
+
+console.log("Length of colors:", colors.length);
+console.log("Length of booleans:", booleans.length);
+console.log("Length of empty array:", emptyArray.length);
+
+shoppingList = ["Milk", "Bread", "Apple"];
+console.log(shoppingList);
+console.log(shoppingList.length);
+shoppingList[1] = "Bananas";
+console.log(shoppingList);
