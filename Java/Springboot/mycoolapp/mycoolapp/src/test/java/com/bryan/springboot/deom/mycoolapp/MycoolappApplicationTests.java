f7636efb4b4aff19b1d986e71c5dd0a35e91c1ae
Bryan Coss Y Leon <bryan.cossyleon10@gmail.com>
2026-02-10 18:59:11 -0700

    Data clean up.

---
+++ Java/Springboot/mycoolapp/mycoolapp/src/test/java/com/bryan/springboot/deom/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.springboot.deom.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
